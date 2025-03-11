2293020c035658eebc42c5fba4472c77019185af
Josh-Petersen <25jpetersen2@go.dsdmail.net>
2025-03-11 03:11:04 +0000

    goofy stuff

--- personalityTest.py
+++ personalityTest.py
@@ -15,11 +15,11 @@ class Page():
 
         #Checks to see if the page is the final page in the list, if so, moves to the show_final_window instead of the next.
         if skillsList.index(self) == len(skillsList) -1:
-            #Asks the question and gives 3 answers, Yes, I dont know, or No. Runs the score function if its yes or no, moves onto the final page if any button is pressed, and destroys this page.
+            #Asks the question and gives 3 answers, Yes, I dont know, or No. Runs the score function if its yes or no, moves onto the final page if any button is pressed.
             tk.Label(window,text = self.key).grid(row=0, column=0, pady= 10)
-            tk.Button(window, text= "Yes", command=lambda: (self.score(True),show_final_window(),window.destroy())).grid(row=1, column=0,pady=10)
-            tk.Button(window, text= "Maybe", command=lambda: (show_final_window(),window.destroy())).grid(row=2, column=0,pady=10)
-            tk.Button(window, text= "No", command=lambda: (self.score(False),show_final_window(),window.destroy())).grid(row=3, column=0,pady=10)
+            tk.Button(window, text= "Yes", command=lambda: (self.score(True),show_final_window())).grid(row=1, column=0,pady=10)
+            tk.Button(window, text= "Maybe", command=lambda: (show_final_window())).grid(row=2, column=0,pady=10)
+            tk.Button(window, text= "No", command=lambda: (self.score(False),show_final_window())).grid(row=3, column=0,pady=10)
         #Runs if the page is not the final page in the list
         else:
             #Asks the question and gives 3 answers, Yes, I dont know, or No. Runs the score function if its yes or no, moves onto the next page if any button is pressed, and destroys this page.
@@ -39,17 +39,17 @@ class Page():
 
 #Displays the final window
 def show_final_window():
-    #Destroys the root window (The one asking if you want to take the test)
+    #Destroys the root window (The one asking if you want to take the test) and all children
     root.destroy()
     #Sets up the page window
-    window = tk.Tk()
-    window.title("Your score")
-    window.geometry("800x500")
+    window1 = tk.Tk()
+    window1.title("Your score")
+    window1.geometry("800x500")
 
     #Displays your scores
-    tk.Label(window, text=weightScoring).grid(row=0, column=0, pady= 10)
+    tk.Label(window1, text=weightScoring).grid(row=0, column=0, pady= 10)
     #Destroys the window
-    tk.Button(window, text="Exit", command=window.destroy).grid(row=1, column=0, pady= 10)
+    tk.Button(window1, text="Exit", command=window1.destroy).grid(row=1, column=0, pady= 10)
 
 #Defines a dictionary that contains the question and respective points earned in the 5 OCEAN catagories, in order they are Openness, Conscientiousness, Extraversion, Agreeableness, and Neuroticism.        
 skills = {"Are you good at COMMUNICATING" : (5,4,5,4,2), "Do you work well in a TEAM?" : (4,5,4,5,2), "Are you good at PROBLEM-SOLVING?" : (5,5,3,3,2), "Are you good at TIME MANAGEMENT": (3,5,2,3,2)}
