1954773596936fe35a03c1307157bfb30c443a3a
StilTheSeal <162468983+StilTheSeal@users.noreply.github.com>
2025-03-11 01:32:29 +0000

    HAAAA

--- personalityTest.py
+++ personalityTest.py
@@ -1,15 +1,46 @@
 import tkinter as tk
 
-openPerson = False
+class Page():
+    def __init__(self):
+        pass
+    def show_window(self):
+        window = tk.Toplevel(root)
+        window.title(key)
+        window.geometry("800x500")
 
-def show_test_window():
-    global test_window
-    test_window = tk.Toplevel(root)
-    test_window.title("Personality Test")
-    test_window.geometry("800x500")
+        tk.Label(window,text = key).grid(row=0, column=0, pady= 10)
+        tk.Button(window, text= "Yes", command=lambda: self.score(True)).grid(row=1, column=0,pady=10)
+        tk.Button(window, text= "No", command=lambda: self.score(False)).grid(row=1, column=1,pady=10)
+    def score(self,boolean):
+        if boolean == True:
+            print("YES")
+        else:
+            print("NO")
 
-    tk.Label(test_window, text="Personality Test").grid(row=0, column=0, pady= 10)
-    tk.Checkbutton(test_window, text="Are you an open person", variable=openPerson).grid(row=1, column=0, pady=10)
+#for key, value in skills.items():
+#        tk.Checkbutton(test_window,text=key, variable = openPerson).grid(row=skillsLocation.index(key), column=0, pady=10)
+
+skills = {"Are you an open person?" : [1,0,0,2,0], "Do you like people?" : [0,1,0,1,1], "Do you enjoy labor?" : [0,0,3,0,0]}
+for key,value in skills.items():
+    skillsButtonate = {}
+skillsLocation = list(skills.keys())
+skillsList = []
+
+for skill in skills:
+    skill = Page()
+    skillsList.append(skill)
+
+print(skillsList)
+
+weightScoring = [0,0,0,0,0]
+
+#def show_test_window():
+    #global test_window
+    #test_window = tk.Toplevel(root)
+    #test_window.title("Personality Test")
+    #test_window.geometry("800x500")
+
+    #tk.Label(test_window, text="Personality Test").grid(row=0, column=0, pady= 10)
     
 
 
@@ -19,7 +50,8 @@ if __name__ == "__main__":
     root.geometry("800x500")
     root.resizable(False, False)
 
-    tk.Button(root, text="Take Test", command=show_test_window).pack(pady=10)
+    tk.Button(root, text="Take Test", command=skillsList[0].show_window).pack(pady=10)
+    #tk.Button(root, text="Take Test", command=show_test_window).pack(pady=10)
     tk.Button(root, text="Exit", command=root.destroy).pack(pady=10)
 
     root.mainloop()
